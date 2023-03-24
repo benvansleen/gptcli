38773a6cc5440bfff54508defd9099dae8809dd8
Ben Van Sleen <benvansleen@gmail.com>
2023-03-24 17:00:26 -0500

    Improve prompt_loop (don't allow infinite error handling)

--- gptcli/interface.py
+++ gptcli/interface.py
@@ -1,4 +1,5 @@
 from os import chdir, system
+from sys import exit
 from subprocess import run, CalledProcessError
 from json import loads
 from gptcli import console
@@ -13,13 +14,20 @@ def prompt_loop(wd, llm, prompt):
         response = loads(response)
         console.print(console.panels(**response))
     except Exception as e:
-        prompt.query('Follow the appropriate JSON formatting!')
+        console.print(f'[bold red]Whoops, I might be stuck!')
+        console.print(response)
+
+        n_errors = prompt.json_error()
+        if n_errors > 2:
+            console.print(f"[bold red]I've had enough of this, goodbye!")
+            exit(1)
         prompt_loop(wd, llm, prompt)
 
     feedback = console.prompt('[bold]Any feedback? (Press enter to run)')
     if feedback:
         prompt.query(feedback)
         prompt_loop(wd, llm, prompt)
+
     else:
         chdir(wd)
         proc = run(
@@ -29,17 +37,17 @@ def prompt_loop(wd, llm, prompt):
             capture_output=True,
         )
         stdout = proc.stdout.decode('utf-8')
+        stderr = proc.stderr.decode('utf-8')
         if proc.returncode:
             console.print('[bold red]Something went wrong')
             console.print(stdout)
-            prompt.query(
-                f'''
-                That command failed with this traceback:
-                {stdout}
-                Provide a new command to fix this error.
-                '''
-            )
-            console.print("[bold green]Let's see if I can figure it out...")
-            prompt_loop(wd, llm, prompt)
+            console.print(stderr)
+            if console.confirm('Do you want me to try to debug the error?'):
+                prompt.debug(stdout, stderr)
+                console.print("[bold green]Let's see if I can figure it out...")
+                prompt_loop(wd, llm, prompt)
+            else:
+                console.print('[bold red]Exiting...')
+                exit(1)
         else:
             console.print(stdout)
